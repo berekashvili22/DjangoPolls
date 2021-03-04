122f30ef8bdd723b6a2d732d3266c23639cf83c0
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-05 00:49:49 +0400

    create serializer for vote

--- PollsProject/api/serializers.py
+++ PollsProject/api/serializers.py
@@ -1,6 +1,7 @@
 # from django.contrib.auth.models import User, Group
 from rest_framework import serializers
-from . models import Question, Choice
+from . models import Question, Choice, Vote
+
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,21 @@ class ChoiceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Choice
         fields = '__all__'
+
+class VoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('__all__')
+        optional_fields = ['user',]
+    
+    def save(self):
+        user = self.context['request'].user
+        choice = self.validated_data['choice']
+
+        vote = Vote.objects.create(
+            user=user,
+            choice=choice
+        ).save()
+        return vote
+
+        
