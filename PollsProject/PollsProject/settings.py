c93cc1b65c1c79c938b3bb33067db2b86d070a74
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-04 14:07:07 +0400

    add installed apps to settings

--- PollsProject/PollsProject/settings.py
+++ PollsProject/PollsProject/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api.apps.ApiConfig',
+    'polls.apps.PollsConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
