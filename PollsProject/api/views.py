da73336bbc23a3e1ff830059a5f4c385932e3d00
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-05 00:50:40 +0400

    create api view for vote

--- PollsProject/api/views.py
+++ PollsProject/api/views.py
@@ -4,8 +4,8 @@ from django.http import JsonResponse
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from . models import Question, Choice
-from . serializers import QuestionSerializer, ChoiceSerializer
+from . models import Question, Choice, Vote
+from . serializers import QuestionSerializer, ChoiceSerializer, VoteSerializer
 from django.shortcuts import get_object_or_404
 
 
@@ -35,15 +35,37 @@ def questionDetail(request, pk):
     question_serializer = QuestionSerializer(question, many=False)
     choices_serializer = ChoiceSerializer(choices, many=True)
 
-    data = {
-        'question': question_serializer.data,
-        'choices': choices_serializer.data,
-    }
+    if question.completed == False:
+        data = {
+            'question': question_serializer.data,
+            'choices': choices_serializer.data,
+        }
+    elif question.completed == True:
+        choices_text = []
+        votes_count = []
+        for choice in choices_serializer.data:
+            choice_text = choice['choice_text']
+            vote_count = Vote.objects.filter(choice__id=choice['id']).count()
+            choices_text.append(choice_text)
+            votes_count.append(vote_count)
+        
+        data = {
+            'question': question_serializer.data,
+            'choices_text': choices_text,
+            'votes_count': votes_count,
+        }
 
     return Response(data)
 
 @permission_classes([IsAuthenticated])
-@api_view(['POST'])
+@api_view(['POST', 'GET'])
 def questionVote(request):
-    pass
+    if request.method == 'GET':
+        votes = Vote.objects.all()
+        serializer = VoteSerializer(votes, many=True)
+    if request.method == 'POST':
+        serializer = VoteSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            serializer.save()  
+    return Response(serializer.data)
  
