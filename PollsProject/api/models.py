6c0aa20cde55eb5567a633ce294accb3220b5cde
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-05 00:49:31 +0400

    create vote model

--- PollsProject/api/models.py
+++ PollsProject/api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -11,7 +12,14 @@ class Question(models.Model):
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
     
     def __str__(self):
         return self.choice_text
+
+class Vote(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f'{self.user}'
+
