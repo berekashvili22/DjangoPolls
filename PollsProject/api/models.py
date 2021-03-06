69b2f824bc54c3588f951451833c4ef7e78f6ff6
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-06 12:48:15 +0400

    make vote fields unique together

--- PollsProject/api/models.py
+++ PollsProject/api/models.py
@@ -19,7 +19,10 @@ class Choice(models.Model):
 class Vote(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('user', 'choice')
     
     def __str__(self):
-        return f'{self.user}'
+        return f'{self.user} on {self.choice.question}  {self.choice}'
 
