63a593083fe08fed844291ae3f04c098b567e71b
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-10 12:04:18 +0400

    create views for frontend

--- PollsProject/polls/views.py
+++ PollsProject/polls/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
 
 def home(request):
     return render(request, 'polls/home.html')
+
+def polls(request):
+    return render(request, 'polls/polls.html')
+
+def pollDetail(request, pk):
+    context = {'pk': pk}
+    return render(request, 'polls/poll-detail.html', context)
