5a479e636ac31646c8515c1e26502675cf299eb3
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-10 12:03:18 +0400

    create front urls

--- PollsProject/polls/urls.py
+++ PollsProject/polls/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('polls/', views.polls, name='polls'),
+    path('poll/<pk>/', views.pollDetail, name='poll-detail'),
 ]
