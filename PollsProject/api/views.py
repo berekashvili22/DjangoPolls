b34cc60a0035bcf86d49afbfd2a99b85ed5c680d
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-10 12:02:54 +0400

    update detail view

--- PollsProject/api/views.py
+++ PollsProject/api/views.py
@@ -27,42 +27,66 @@ def questionList(request):
 
 @api_view(['GET'])
 def questionDetail(request, pk):
+    # get question and question choices
     question = Question.objects.get(pk=pk)
     choices = Choice.objects.filter(question=question)
 
+    # serialize data
     question_serializer = QuestionSerializer(question, many=False)
     choices_serializer = ChoiceSerializer(choices, many=True)
 
-    # 
+    # get reqeust user
     user = request.user
-    vote = Vote.objects.filter(user=user, choice__in=choices)
-    current_user_vote_status = None
+
+    # check if user voted on currect question
+    vote = Vote.objects.filter(user=user, choice__in=choices).first()
+    canVote = None
+    # if vote exists that means user has already on that question so set canVote to false
     if vote:
-        current_user_vote_status = 'you already voted'
+        canVote = False
+        # get choice id to check voted radio button
+        userChoiceId = vote.choice.id
+    # else set canVote to true
     else:
-        current_user_vote_status = 'you can vote'
-    if question.completed == False:
-        data = {
-            'question': question_serializer.data,
-            'choices': choices_serializer.data,
-        }
-    elif question.completed == True:
-        choices_text = []
-        votes_count = []
-        for choice in choices_serializer.data:
+        canVote = True
+        userChoiceId = None
+    
+    # check if question is completed if not , if its complted set canVote to false  
+    if question.completed == True:
+        canVote = False;
+
+    # ------------------------
+    #      this data will be user for data visualization (with zingcharts)
+
+    # create empty lists for choice data
+    choices_text = []
+    votes_count = []
+
+    # loop through currenct question choices
+    for choice in choices_serializer.data:
+            # get choice text for each choice
             choice_text = choice['choice_text']
+            # get vote count for each choice
             vote_count = Vote.objects.filter(choice__id=choice['id']).count()
+
+            # add choice data to lists
             choices_text.append(choice_text)
             votes_count.append(vote_count)
-        
-        data = {
-            'question': question_serializer.data,
-            'choices_text': choices_text,
-            'votes_count': votes_count,
-            'current_user_vote_status': current_user_vote_status,
-        }
+
+    # ------------------------
+
+    # create data for front
+    data = {
+        'question': question_serializer.data,
+        'choices': choices_serializer.data,
+        'canVote': canVote,
+        'userChoiceId': userChoiceId,
+        'choices_text': choices_text,
+        'votes_count': votes_count,
+    }
 
     return Response(data)
+
 
 @permission_classes([IsAuthenticated])
 @api_view(['POST', 'GET'])
