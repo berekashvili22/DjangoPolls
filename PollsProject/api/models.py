cee2dc827dc8333d393017ac69fcb811d2aa1de6
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-11 23:20:50 +0400

    create properties for poll

--- PollsProject/api/models.py
+++ PollsProject/api/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
+
+
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -8,6 +12,17 @@ class Question(models.Model):
 
     def __str__(self):
         return self.question_text
+    
+    @property
+    def was_published_recently(self):
+        now = timezone.localtime(timezone.now())
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
+    @property
+    def was_bulished_in_present(self):
+        now = timezone.localtime(timezone.now())
+        return self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
