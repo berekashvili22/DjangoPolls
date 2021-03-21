e95bb8f29f79d0ffa77b4d86e570a72bb7de10c2
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-21 23:41:31 +0400

    create dailyquestion's url path

--- PollsProject/polls/urls.py
+++ PollsProject/polls/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('today/', views.poll, name='today'),
     path('polls/', views.polls, name='polls'),
     path('poll/<pk>/', views.pollDetail, name='poll-detail'),
 ]
