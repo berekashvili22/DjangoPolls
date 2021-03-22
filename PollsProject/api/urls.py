cb79fe95a680af075bd6c565576affa49d3b32e3
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-22 22:47:07 +0400

    add new classbased veiw to urlpath

--- PollsProject/api/urls.py
+++ PollsProject/api/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.apiOverview, name='api-overview'),
-    path('question-list/', views.questionList, name='question-list'),
+    path('question-list/', (views.questionList).as_view(), name='question-list'),
     path('question-detail/<str:pk>/', views.questionDetail, name='question-detail'),
     path('question-vote/', views.questionVote, name='question-vote'),
     path('question-daily/', views.questionOfTheDay, name='question-daily'),
