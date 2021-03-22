19817a8b6f37ea0bea44c7fb65fdab622bf2423c
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-22 22:47:27 +0400

    add about page url path

--- PollsProject/polls/urls.py
+++ PollsProject/polls/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
     path('today/', views.poll, name='today'),
     path('polls/', views.polls, name='polls'),
     path('poll/<pk>/', views.pollDetail, name='poll-detail'),
