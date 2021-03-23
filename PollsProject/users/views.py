79cfaccbc9c71680fbe52fe436e5edbbcdb25f9d
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-23 21:59:18 +0400

    create custom views for auth

--- PollsProject/users/views.py
+++ PollsProject/users/views.py
@@ -1,18 +1,41 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from .forms import UserRegisterForm
+from .forms import CreateUserForm
+
+from django.contrib.auth import authenticate, login, logout
 
 def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
+        form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f'Your account has been created, Log in now .')
+            messages.success(request, f'Your account has been created, Log in now')
             return redirect('login')
 
     else:
-        form = UserRegisterForm()
+        form = CreateUserForm()
     return render(request, 'users/register.html', {'form': form})
 
-# def profile(reques
+def loginPage(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, f'Welcome {user.first_name}, You are logged in')
+            return redirect('today')
+        else:
+            messages.info(request, f'Username or Password is incorrect')
+
+    return render(request, 'users/login.html')
+
+
+def logoutUser(request):
+    logout(request)
+    messages.info(request, f'You\'ve been logged out')
+    return redirect('login')
