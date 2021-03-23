da78be6fe790aca3970a0f24d8132e06b9b65c06
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-23 22:00:01 +0400

    update urls for auth pages

--- PollsProject/PollsProject/urls.py
+++ PollsProject/PollsProject/urls.py
@@ -25,8 +25,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('login/', user_views.loginPage, name='login'),
+    path('logout/', user_views.logoutUser, name='logout'),
     path('api/', include('api.urls')),
     path('', include('polls.urls')),
 ]
