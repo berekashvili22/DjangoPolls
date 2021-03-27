732fc014082c947e1329db376cc3cdbecf3d19e6
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-27 16:48:01 +0400

    js for responsive navbar

--- PollsProject/polls/static/polls/js/navbar.js
+++ PollsProject/polls/static/polls/js/navbar.js
@@ -1,22 +1,16 @@
 const navSlide = ()=> {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.navigation-wrap');
-    const navLinks = document.querySelectorAll('.nav-links-ul li');
+    const burger = document.querySelector('.navbar__burger');
+    const nav = document.querySelector('.navbar');
+    var body = document.getElementsByTagName('body')[0];
 
     burger.addEventListener('click', ()=>{
-        // toggle
-        nav.classList.toggle('nav-active');
-        // links animation
-        navLinks.forEach((link, index) => {
-            if(link.style.animation) {
-                link.style.animation = ''
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        // burger animation
+        body.classList.toggle('hide-flow');
+        nav.classList.toggle('responsive')
         burger.classList.toggle('toggleBurg');
+
+
     });
 }
 
 navSlide();
+
