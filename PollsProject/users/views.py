3f61f1c990e764ff8b324d3bbfbfda973889b205
Tornike Berekashvili <tornike.berekashvili@pm.me>
2021-03-27 16:48:27 +0400

    some auth logic

--- PollsProject/users/views.py
+++ PollsProject/users/views.py
@@ -5,32 +5,36 @@ from .forms import CreateUserForm
 from django.contrib.auth import authenticate, login, logout
 
 def register(request):
-    if request.method == 'POST':
-        form = CreateUserForm(request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, f'Your account has been created, Log in now')
-            return redirect('login')
-
+    if request.user.is_authenticated:
+        return redirect('home')
     else:
-        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, f'Your account has been created, Log in now')
+                return redirect('login')
+
+        else:
+            form = CreateUserForm()
     return render(request, 'users/register.html', {'form': form})
 
 def loginPage(request):
     if request.user.is_authenticated:
         return redirect('home')
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
 
-        user = authenticate(request, username=username, password=password)
+            user = authenticate(request, username=username, password=password)
 
-        if user is not None:
-            login(request, user)
-            messages.success(request, f'Welcome {user.first_name}, You are logged in')
-            return redirect('today')
-        else:
-            messages.info(request, f'Username or Password is incorrect')
+            if user is not None:
+                login(request, user)
+                messages.success(request, f'Welcome {user.first_name}, You are logged in')
+                return redirect('today')
+            else:
+                messages.info(request, f'Username or Password is incorrect')
 
     return render(request, 'users/login.html')
 
